dc47154a5cff12ec80c891d895c7bd071c38f0ca
Massimiliano Berardi <massimiliano@syntho.ai>
2022-12-04 13:42:30 +0100

    testing chart

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,54 +1,109 @@
-async function drawChart() {
-        // TODO: this is a test for the API
-        d3.json("http://localhost:5000/api/v1/match_elos").then( data => {
-                console.log(data[0]);
-        })
-
-        // set the dimensions and margins of the graph
-        const margin = {top: 30, right: 30, bottom: 70, left: 60},
+async function drawEloMatch() {
+    // set the dimensions and margins of the graph
+    const margin = {top: 10, right: 30, bottom: 30, left: 40},
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
-        // append the svg object to the body of the page
-        const svg = d3.select("#my_dataviz")
+    // append the svg object to the body of the page
+    const svg = d3.select("#eloPerMatch")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            `translate(${margin.left},${margin.top})`);
+    d3.json("http://localhost:5000/api/v1/match_elos").then(data => {
+        const x = d3.scaleLinear()
+            .domain([0, d3.max(data, function (d) {
+                return +d.average_rating
+            })])
+            .range([0, width]);
+        svg.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x));
+        // set the parameters for the histogram
+        const histogram = d3.layout.histogram()
+            .value(function (d) {
+                return d.average_rating;
+            })   // I need to give the vector of value
+            .domain(x.domain())  // then the domain of the graphic
+            .thresholds(x.ticks(data.length / 100)); // then the numbers of bins
+        // And apply this function to data to get the bins
+        const bins = histogram(data.average_rating);
+
+        // Y axis: scale and draw:
+        const y = d3.scaleLinear().range([height, 0]);
+        y.domain([0, d3.max(bins, function (d) {
+            return d.length;
+        })]);   // d3.hist has to be called before the Y axis obviously
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // append the bar rectangles to the svg element
+        svg.selectAll("rect")
+            .data(bins)
+            .join("rect")
+            .attr("x", 1)
+            .attr("transform", function (d) {
+                return `translate(${x(d.x0)} , ${y(d.length)})`
+            })
+            .attr("width", function (d) {
+                return x(d.x1) - x(d.x0) - 1
+            })
+            .attr("height", function (d) {
+                return height - y(d.length);
+            })
+            .style("fill", "#69b3a2")
+    });
+}
+
+async function drawChart() {
+    // set the dimensions and margins of the graph
+    const margin = {top: 30, right: 30, bottom: 70, left: 60},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    const svg = d3.select("#my_dataviz")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
-        // Parse the Data
-        d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv").then( function(data) {
+    // Parse the Data
+    d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv").then(function (data) {
 
         // X axis
         const x = d3.scaleBand()
-        .range([ 0, width ])
-        .domain(data.map(d => d.Country))
-        .padding(0.2);
+            .range([0, width])
+            .domain(data.map(d => d.Country))
+            .padding(0.2);
         svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "translate(-10,0)rotate(-45)")
+            .style("text-anchor", "end");
 
         // Add Y axis
         const y = d3.scaleLinear()
-        .domain([0, 13000])
-        .range([ height, 0]);
+            .domain([0, 13000])
+            .range([height, 0]);
         svg.append("g")
-        .call(d3.axisLeft(y));
+            .call(d3.axisLeft(y));
 
         // Bars
         svg.selectAll("mybar")
-        .data(data)
-        .join("rect")
-        .attr("x", d => x(d.Country))
-        .attr("y", d => y(d.Value))
-        .attr("width", x.bandwidth())
-        .attr("height", d => height - y(d.Value))
-        .attr("fill", "#69b3a2")
+            .data(data)
+            .join("rect")
+            .attr("x", d => x(d.Country))
+            .attr("y", d => y(d.Value))
+            .attr("width", x.bandwidth())
+            .attr("height", d => height - y(d.Value))
+            .attr("fill", "#69b3a2")
     })
 }
 
+drawEloMatch();
 drawChart();
