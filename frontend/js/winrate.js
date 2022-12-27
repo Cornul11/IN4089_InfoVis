f5d06e80ef8ec24bcfa72e469687d17d8f5e8185
Massimiliano Berardi <massimiliano@syntho.ai>
2022-12-27 18:38:44 +0100

    working on winrate graph, needs ticks to be taken from data directly, remove entries with 0 victories and make it updatable, figure out what to do with no civ selected

--- frontend/js/winrate.js
+++ frontend/js/winrate.js
@@ -13,24 +13,25 @@ class WinRatePatch {
             .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
-        //Read the data
-        d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/connectedscatter.csv",
+        //Read the data TODO: this is just an example for formatting
+        d3.csv("http://localhost:5000/api/v1/winrate_civ?civ=Franks",
             // When reading the csv, I must format variables:
             function (d) {
-                return {date: d3.timeParse("%Y-%m-%d")(d.date), value: d.value}
+                return {patch: d.patch, amount: d.amount}
             }).then(
             // Now I can use this dataset:
             function (data) {
-                // Add X axis --> it is a date format
-                const x = d3.scaleTime()
-                    .domain(d3.extent(data, d => d.date))
-                    .range([0, width]);
+                // Add X axis
+                const x = d3.scaleBand()
+                    .range([0, width])
+                    .domain([35584, 36202, 36906, 37650, 37906, 39284, 39515, 40220, 40874, 41855, 42848, 43210]);
+
                 svg.append("g")
                     .attr("transform", "translate(0," + height + ")")
                     .call(d3.axisBottom(x));
                 // Add Y axis
                 const y = d3.scaleLinear()
-                    .domain([8000, 9200])
+                    .domain([0, d3.max(data, d => d.amount)])
                     .range([height, 0]);
                 svg.append("g")
                     .call(d3.axisLeft(y));
@@ -41,8 +42,8 @@ class WinRatePatch {
                     .attr("stroke", "#69b3a2")
                     .attr("stroke-width", 1.5)
                     .attr("d", d3.line()
-                        .x(d => x(d.date))
-                        .y(d => y(d.value))
+                        .x(d => x(parseInt(d.patch)))
+                        .y(d => y(d.amount))
                     )
                 // Add the points
                 svg
@@ -50,8 +51,8 @@ class WinRatePatch {
                     .selectAll("dot")
                     .data(data)
                     .join("circle")
-                    .attr("cx", d => x(d.date))
-                    .attr("cy", d => y(d.value))
+                    .attr("cx", d => x(parseInt(d.patch)))
+                    .attr("cy", d => y(d.amount))
                     .attr("r", 5)
                     .attr("fill", "#69b3a2")
             })
